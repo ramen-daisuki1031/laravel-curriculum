7c741230dbf7ede6dbe2bb671cc798dc7582d92e
ramen-daisuki1031 <sannabe1031@gmail.com>
2021-05-30 14:54:47 +0900

    詳細画面について

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,11 @@ class PostController extends Controller
 {
     public function index(Post $post)
    {
-    return view('index')->with(['posts' => $post->getPaginateByLimit(1)]);
+    return view('index')->with(['posts' => $post->getPaginateByLimit()]);
+   }
+   
+   public function show(Post $post)
+   {
+       return view('show')->with(['post' => $post]);
    }
 }
