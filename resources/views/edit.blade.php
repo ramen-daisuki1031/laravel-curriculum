7dd846ec35a51388d683de248424cbc962206f82
ramen-daisuki1031 <sannabe1031@gmail.com>
2021-06-04 20:38:22 +0900

    ブログ投稿の編集

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Posts</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        <h1 class="title">
+        </h1>
+         <form action="/posts/{{ $post->id }}" method="POST">
+            {{ csrf_field() }}
+             @method('PUT')
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ $post->title }}">
+                 <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
+            </div>
+            <div class="body">
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{ $post->body }}</textarea>
+                 <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
+            </div>
+            <input type="submit" value="update"/>
+        </form>
+        <div class="footer">
+            <a href="/posts/{{  $post->id }}">back</a>
+        </div>
+    </body>
+</html>
